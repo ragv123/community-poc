265d4a61f822f23e5c4ca8005987d01b3dcc19d8
rajeev <rajeev@tothenew.com>
2017-06-30 13:01:10 +0530

    Added ListProvider for custom blog resource

--- bundle/src/main/java/com/ttnd/community/journal/poc/SCFJournalCreateExtension.java
+++ bundle/src/main/java/com/ttnd/community/journal/poc/SCFJournalCreateExtension.java
@@ -1,8 +1,6 @@
 package com.ttnd.community.journal.poc;
-public class SCFJournalCreateExtension {
-	
-}
-/*import java.util.Arrays;
+
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -43,77 +41,71 @@ public class SCFJournalCreateExtension implements JournalOperationExtension {
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return "custom journal";
+		return "custom blog";
 	}
 
 	@Override
 	public List<JournalOperation> getOperationsToHookInto() {
-		return Arrays.asList(JournalOperation.CREATE,JournalOperation.UPDATE, JournalOperation.DELETE,JournalOperation.UPLOADIMAGE);
+		return Arrays.asList(JournalOperation.CREATE, JournalOperation.UPDATE, JournalOperation.DELETE,
+				JournalOperation.UPLOADIMAGE);
 	}
 
 	@Override
 	public void beforeAction(Operation paramOperation, Session paramSession, Resource paramResource,
 			Map<String, Object> paramMap) throws OperationException {
-		//paramResource.getResourceResolver().adaptTo(Session.class);
+		ValueMap resourceMap = paramResource.adaptTo(ValueMap.class);
 		ResourceResolver resourceResolver = paramResource.getResourceResolver();
+		try {
+			if (ResourceUtil.isA(paramResource, "blog/components/hbs/journal")) {
+				String[] selectedGroup = resourceMap.get("oauth.create.users.groups", new String[0]);
+				Iterator<Group> groups = getUserGroups(paramResource, resourceResolver);
+				List<String> userGroups = Arrays.asList(selectedGroup);
+				while (groups.hasNext()) {
+					if (userGroups.contains(groups.next().getID())) {
+						paramMap.put("approved", Boolean.valueOf(true));
+						break;
+					}
+				}
+			} else if (ResourceUtil.isA(paramResource, "blog/components/hbs/entry_topic") || ResourceUtil.isA(paramResource, "social/journal/components/hbs/comment")) {
+				String root = resourceMap.get("social:rootCommentSystem", String.class);
+				ValueMap rootResourceMap = resourceResolver.resolve(root).adaptTo(ValueMap.class);
+				/*String[] selectedGroup = rootResourceMap.get("oauth.create.users.groups", new String[0]);
+				Iterator<Group> groups = getUserGroups(paramResource, resourceResolver);
+				List<String> userGroups = Arrays.asList(selectedGroup);
+				while (groups.hasNext()) {
+					if (userGroups.contains(groups.next().getID())) {
+						autoModerateComments(paramMap);
+						break;
+					}
+				}*/
+				if(rootResourceMap.get("premoderatedComments", false)){
+					paramMap.put("approved", Boolean.valueOf(true));
+					paramMap.put("isFlagged", Boolean.valueOf(false));
+					paramMap.put("isSpam", Boolean.valueOf(false));
+					paramMap.put("isFlaggedHidden", Boolean.valueOf(false));
+					paramMap.put("moderate", Boolean.valueOf(true));
+				}
+			}
+		} catch (RepositoryException e) {
+			LOG.error("Repository Exception : ", e);
+		}
+	}
+
+	private Iterator<Group> getUserGroups(Resource paramResource, ResourceResolver resourceResolver)
+			throws RepositoryException {
 		UserProperties up = (UserProperties) resourceResolver.adaptTo(UserProperties.class);
 		String userIdentifier = (up == null) ? null : up.getAuthorizableID();
-		System.out.println("userIdentifier : "+ userIdentifier);
-		String [] selectedGroup = paramResource.adaptTo(ValueMap.class).get("oauth.create.users.groups",new String[0]);
-		System.out.println("selectedGroup : "+ selectedGroup);
-		System.out.println("group from map : "+ paramMap.get("oauth.create.users.groups"));
-		
-		
-		
-		
 		UserManager userManager = resourceResolver.adaptTo(UserManager.class);
-         to get the current user  
-        Authorizable auth;
-		try {
-			auth = userManager.getAuthorizable(userIdentifier);
-			 to get the groups it is member of  
-	          Iterator<Group> groups = auth.memberOf();
-	          //String [] selectedGroup = targetCommentSystemResource.adaptTo(ValueMap.class).get("oauth.create.users.groups",new String[0]);
-	          //Arrays.sort(selectedGroup);
-	          List<String> userGroups = Arrays.asList(selectedGroup);
-	          while(groups.hasNext()){
-	        	  String grpId = groups.next().getID();
-	        	  System.out.println("group id : "+grpId);
-	        	  if(userGroups.contains(grpId)){
-			          System.out.println("result true");
-			          paramMap.put("approved", Boolean.valueOf(true));
-			          break;
-	        	  }else{
-	        		  System.out.println("result false");
-	        	  }
-	          }
-		} catch (RepositoryException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e);
-		} 
-		
-		
-		
-		
-		
-		if (ResourceUtil.isA(paramResource, "social/journal/components/hbs/journal")) {
-			LOG.info("Approved Value" + paramMap);
-        	LOG.info("User is : " + paramSession.getUserID());
-        	Boolean isApproved = true;
-        	paramMap.put("approved", isApproved);
-            if(paramSession.getUserID().equals("admin")){
-            	paramMap.put("approved", isApproved);
-            }
-		}
-		
+		Authorizable auth;
+		auth = userManager.getAuthorizable(userIdentifier);
+		return auth.memberOf();
 	}
 
 	@Override
 	public void afterAction(Operation paramOperation, Session paramSession, JournalEntryComment paramT,
 			Map<String, Object> paramMap) throws OperationException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-*/
